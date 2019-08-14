a3221ef53721c161f43072d07d02399903687b2c
Nestor <nestoroprysk@gmail.com>
2019-08-14 22:41:33 +0300

    static asserts provided

--- Compose.hpp
+++ Compose.hpp
@@ -2,10 +2,15 @@
 
 #include <utility>
 
+#define INVOCABLE_ASSERTION_FAILED_ERROR_MESSAGE\
+    "An object should be invokable with the provided arguments"
+
 template <typename F>
 auto compose(F&& f)
 {
     return [a = std::forward<F>(f)](auto&&... args){
+        static_assert(std::is_invocable_v<F, decltype(args)...>,
+            INVOCABLE_ASSERTION_FAILED_ERROR_MESSAGE);
         return a(std::forward<decltype(args)>(args)...);
     };
 }
@@ -16,6 +21,15 @@ auto compose(F1&& f1, F2&& f2, Fs&&... fs)
     return compose(
         [first = std::forward<F1>(f1), second = std::forward<F2>(f2)]
         (auto&&... args){
+            static_assert(std::is_invocable_v<F1, decltype(args)...>,
+                INVOCABLE_ASSERTION_FAILED_ERROR_MESSAGE
+            );
+            static_assert(
+                std::is_invocable_v<F2,
+                    std::result_of_t<F1(decltype(args)...)>
+                >,
+                INVOCABLE_ASSERTION_FAILED_ERROR_MESSAGE
+            );
             return second(first(std::forward<decltype(args)>(args)...));
         },
         std::forward<Fs>(fs)...
